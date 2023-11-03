6f0f1bf52175ded63330aeb3e13b7185df0b72c8
mary_waiyego <waiyegomary55@gmail.com>
2023-11-03 15:51:03 +0300

    merged

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,196 +1,253 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './ChatBot.js';
-import './chatbot.css';
-//import botIcon from './imagess/chatbot icon.jpg';
+// import React, { useState, useEffect, useRef } from 'react';
+// import './ChatBot.js';
+// import './chatbot.css';
+// //import botIcon from './imagess/chatbot icon.jpg';
+// //import logo from '.';
+// function ChatBot() {
+//     const topic = useState('OvesSmart chat')[0]; // You can default it to any topic you like
+//     //const [chatBotActive, setChatBotActive] = useState(false);
+//     const [messages, setMessages] = useState([]);
+//     const [input, setInput] = useState('');
+//     const [isTyping, setIsTyping] = useState(false);
+//     const [ws, setWs] = useState(null);
+//     const messagesEndRef = useRef(null);
+//     const scrollToBottom = () => {
+//         if (messagesEndRef.current) {
+//             messagesEndRef.current.scrollIntoView(true);
+//         }
+//     };
 
-//import logo from '.';
+//     useEffect(() => {
+//         const websocket = new WebSocket('wss://dev-chatbot.omnivoltaic.com/ws');
+//         setWs(websocket);
+//         websocket.onopen = () => {
+//             console.log("Connected to the WebSocket server");
+//         };
+//         websocket.onmessage = (event) => {
+//             const botMessage = event.data;
+//             setMessages((prevMessages) => [
+//                 ...prevMessages,
+//                 { type: 'bot', text: botMessage }
+//             ]);
+//             setIsTyping(true);
+//         };
+//         websocket.onerror = (event) => {
+//             console.error("WebSocket Error:", event);
+//         };
+//         websocket.onclose = () => {
+//             console.log("WebSocket connection closed");
+//         };
+//         return () => {
+//             websocket.close();
+//         };
+//     }, []);
+//     useEffect(() => {
+//         setTimeout(scrollToBottom, 0);
+//     }, [messages]);
+
+//     const handleKeyDown = (e) => {
+//         if (e.key === 'Enter') {
+//             handleSubmit();
+//         }
+//     };
+//     const handleSubmit = () => {
+//         if (input.trim() === '') return;
+//         setMessages((prevMessages) => [
+//             ...prevMessages,
+//             { type: 'user', text: input.trim() }
+//         ]);
+//         // console.log(ws);
+//         // console.log(input)
+//         if (ws) {
+//             ws.send(input.trim()); // Send user's input to the backend
+//             setIsTyping(true);
+//             setInput('');  // Clear input field
+//             setTimeout(() => {
+//                 setIsTyping(false);
+//             }, 1000);
+//         }
+//     };
+
+//     // document.getElementById('chatbot-icon').onclick = function () {
+//     //     // ... same as above ...
+//     // };
+//     // window.onclick = function (event) {
+//     //     // ... same as above ...
+//     // };
+//     return (
+//         // <>
+//         //     {chatBotActive ? (
+//         //         <ChatBot />
+//         //     ) : (
+//         //             <img
+//         //                 src={botIcon}
+//         //                 alt="Open Chatbot"
+//         //                 onClick={() => setChatBotActive(true)}
+//         //                 style={{ cursor: 'pointer', position: 'fixed', bottom: '20px', right: '20px' }}
+//         //             />
+//         //         )}
+//         <div className="chatbot-container">
+//             <div className="chatbot-header"> {/* Add this block for the header */}
+//                 Topic: {topic}
+//             </div>
+//             <div className="messages-container">
+//                 {messages.map((message, index) => (
+//                     <div key={index} className={`message ${message.type}`}>
+//                         {message.text}
+//                     </div>
+//                 ))}
+//                 {isTyping && (
+//                     <div className="typing-indicator">
+//                         <span></span>
+//                         <span></span>
+//                         <span></span>
+//                     </div>
+//                     // When sending a message or waiting for a response
+//                     //document.querySelector('.typing-indicator').classList.add('active');
+//                     // Once the response is received and displayed
+//                     //document.querySelector('.typing-indicator').classList.remove('active');
+//                 )}
+//                 <div ref={messagesEndRef} />
+//             </div>
+//             <div className="input-container">
+//                 <input
+//                     value={input}
+//                     onChange={e => setInput(e.target.value)}
+//                     onKeyDown={handleKeyDown}
+//                     placeholder="Ask me anything..." />
+//                 <button onClick={handleSubmit}>Submit</button>
+//             </div>
+//             {/* <div className="input-container">
+//                  <input value={input} onChange={e => setInput(e.target.value)} placeholder="Ask me anything..." />
+//                  <button onClick={handleSubmit}>Submit</button>
+//              </div> */}
+//         </div>
+//         // </>
+//     );
+// }
+// export default ChatBot;
+
+import React, { useState, useEffect, useRef } from 'react';
+//  import Container from 'react-bootstrap/Container';
+//  import Row from 'react-bootstrap/Row';
+//  import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './chatbot.css';
+
 function ChatBot() {
-    const topic = useState('OvesSmart chat')[0]; // You can default it to any topic you like
-    //const [chatBotActive, setChatBotActive] = useState(false);
+    const topic = useState('OvesSmart chat')[0];
     const [messages, setMessages] = useState([]);
     const [input, setInput] = useState('');
     const [isTyping, setIsTyping] = useState(false);
     const [ws, setWs] = useState(null);
     const messagesEndRef = useRef(null);
+
     const scrollToBottom = () => {
         messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
     };
+
     useEffect(() => {
         const websocket = new WebSocket('wss://dev-chatbot.omnivoltaic.com/ws');
         setWs(websocket);
-        websocket.onopen = () => {
-            console.log("Connected to the WebSocket server");
-        };
+        websocket.onopen = () => console.log("Connected to the WebSocket server");
         websocket.onmessage = (event) => {
             const botMessage = event.data;
-            setMessages((prevMessages) => [
-                ...prevMessages,
-                { type: 'bot', text: botMessage }
-            ]);
+            setMessages((prevMessages) => [...prevMessages, { type: 'bot', text: botMessage }]);
             setIsTyping(true);
         };
-        websocket.onerror = (event) => {
-            console.error("WebSocket Error:", event);
-        };
-        websocket.onclose = () => {
-            console.log("WebSocket connection closed");
-        };
-        return () => {
-            websocket.close();
-        };
+        websocket.onerror = (event) => console.error("WebSocket Error:", event);
+        websocket.onclose = () => console.log("WebSocket connection closed");
+        return () => websocket.close();
     }, []);
 
     useEffect(() => {
-        // Check if the last message is from the user, if so, scroll to bottom
-        if (messages.length > 0 && messages[messages.length - 1].type === 'user') {
-            scrollToBottom();
-        }
-    }, [messages]); // Only re-run the effect if new messages are added to the list
-
+        scrollToBottom();
+    }, [messages]);
 
     const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            handleSubmit();
-        }
+        if (e.key === 'Enter') handleSubmit();
     };
+
     const handleSubmit = () => {
         if (input.trim() === '') return;
         const newMessage = { type: 'user', text: input.trim() };
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            newMessage
-        ]);
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
         if (ws) {
-            ws.send(input.trim()); // Send user's input to the backend
+            ws.send(input.trim());
             setIsTyping(true);
-            setInput('');  // Clear input field
-            // Call scrollToBottom after a slight delay to ensure the message has been rendered
+            setInput('');
             setTimeout(() => {
                 setIsTyping(false);
-                scrollToBottom();
-            }, 100);
+                // scrollToBottom();
+                // function ChatBot() {
+            },1000);
         }
     };
 
-    return (
-        // <>
-        //     {chatBotActive ? (
-        //         <ChatBot />
-        //     ) : (
-        //             <img
-        //                 src={botIcon}
-        //                 alt="Open Chatbot"
-        //                 onClick={() => setChatBotActive(true)}
-        //                 style={{ cursor: 'pointer', position: 'fixed', bottom: '20px', right: '20px' }}
-        //             />
-        //         )}
-        <div className="chatbot-container">
-            <div className="chatbot-header">
-                Topic: {topic}
-            </div>
-            <div className="messages-container">
-                {messages.map((message, index) => {
-                    // Check if the message is from the user
-                    const isUserMessage = message.type === 'user';
-                    // Determine if the previous message was from the same sender
-                    const sameSenderAsPrevious = index > 0 && message.type === messages[index - 1].type;
-                    // Determine if the next message is from a different sender
-                    const nextMessageDifferent = index < messages.length - 1 && message.type !== messages[index + 1].type;
-
-                    // We want to start a new container for the first message
-                    // or when the sender changes
-                    if (index === 0 || !sameSenderAsPrevious) {
-                        return (
-                            <Container key={index}>
-                                <Row>
-                                    <Col className={`message ${message.type}`}>
-                                        <br/>{message.text}
-                                    </Col>
-                                </Row>
-                                {/* If the next message is from a different sender or it's the last message, we close the container */}
-                                {nextMessageDifferent || index === messages.length - 1 ? null : <br />}
-                            </Container>
-                        );
-                    } else {
-                        // If the message is not the first and the sender hasn't changed,
-                        // we continue in the same container without adding a new one.
-                        return (
-                            <React.Fragment key={index}>
-                                <Row>
-                                    <Col className={`message ${message.type}`}>
+                return (
+                    <div className="chatbot-container">
+                        <div className="chatbot-header">Topic: {topic}</div>
+                        <div className="messages-container">
+                            {messages.map((message, index) => (
+                                <div key={index} className={`message-row ${message.type}`}>
+                                    <div className={`message ${message.type}`}>
                                         {message.text}
-                                    </Col>
-                                </Row>
-                                {/* We only add a bottom margin if the next message is from a different sender */}
-                                {nextMessageDifferent ? null : <br />}
-                            </React.Fragment>
-                        );
-                    }
-                })}
-                {/* {messages.map((message, index) => {
+                                    </div>
+                                </div>
+                            ))}
+                            <div ref={messagesEndRef} />
+                        </div>
 
-                    console.log(message)
-                    return (
-                        <Container>
-                            {
-                                message.type == 'user' ? <Row>
-                                    <Col>
-                                        <div key={index} className={`message user`}>
-                                            <br />{message.text}
-                                        </div>
-                                    </Col>
-                                </Row> : null
-                            }
-                            <br />
-                            <Row>
-                                <Col>
-                                    {
-                                        message.type == 'bot' ? <div key={index} className={`message bot`}>
-                                            <br />{message.text}
-                                        </div> : null
-                                    }
+                        {/* <div className="messages-container">
+                 {messages.map((message, index) => (
+                    <Container key={index}>
+                        <Row>
+                            <Col
+                                xs={12}
+                                className={`message ${message.type}`}
+                                style={{ textAlign: message.type === 'user' ? 'right' : 'left' }}
+                            >
+                                {message.text}
+                            </Col>
+                             </Row>
+                            </Container>
+                          ))}
+                          <div ref={messagesEndRef} />
+                          </div> */}
 
-                                </Col>
-
-                            </Row>
-                        </Container>
-                    )
-                })} */}
-                {isTyping && (
-                    <div className="typing-indicator">
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                        <div className="messages-container">
+                     {messages.map((message, index) => (
+                     <div key={index} className={`message ${message.type}`}>
+                         {message.text}
+                     </div>
+                 ))}
+                 {isTyping && (
+                     <div className="typing-indicator">
+                         <span></span>
+                         <span></span>
+                         <span></span>
+                     </div>
+                     // When sending a message or waiting for a response }
+                     //document.querySelector('.typing-indicator').classList.add('active');
+                     // Once the response is received and displayed
+                     //document.querySelector('.typing-indicator').classList.remove('active');
+                 )}
+                 <div ref={messagesEndRef} />
+             </div>
+                        <div className="input-container">
+                            <input
+                                value={input}
+                                onChange={e => setInput(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                                placeholder="Ask me anything..."
+                            />
+                            <button onClick={handleSubmit}>Submit</button>
+                        </div>
                     </div>
-                )}
-                {/* {messages.map((message, index) => (
-                    <div key={message.id || `msg-${index}`} className={`message ${message.type}`}>
-                        {message.text}
-                    </div>
-                ))} */}
+                );
 
-                <div ref={messagesEndRef} /> {/* This is the element used to scroll to */}
-            </div>
-            <div className="input-container">
-                <input
-                    value={input}
-                    onChange={e => setInput(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Ask me anything..." />
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
-        </div>
 
-        /* <div className="input-container">
-             <input value={input} onChange={e => setInput(e.target.value)} placeholder="Ask me anything..." />
-             <button onClick={handleSubmit}>Submit</button>
-         </div> */
 
-        // </>
-
-    );
 }
+
 export default ChatBot;
--- src/index.js
+++ src/index.js
@@ -7,20 +7,18 @@ import reportWebVitals from './reportWebVitals';
 import AuthPage from './AuthPage';
 
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    if (!isAuthenticated) {
-      return <AuthPage onAuthenticated={setIsAuthenticated} />;
-    }
-     return <ChatBot />;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  if (!isAuthenticated) {
+    return <AuthPage onAuthenticated={setIsAuthenticated} />;
+  }
+  return <ChatBot />;
 }
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
