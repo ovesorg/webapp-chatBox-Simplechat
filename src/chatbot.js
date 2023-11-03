91c169819850336f7abf02680324e91916bafe19
mary_waiyego <waiyegomary55@gmail.com>
2023-11-03 08:07:50 +0300

    start changes

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './ChatBot.js';
 import './chatbot.css';
 //import botIcon from './imagess/chatbot icon.jpg';
@@ -89,11 +92,73 @@ function ChatBot() {
                 Topic: {topic}
             </div>
             <div className="messages-container">
-                {messages.map((message, index) => (
-                    <div key={index} className={`message ${message.type}`}>
-                        {message.text}
-                    </div>
-                ))}
+                {messages.map((message, index) => {
+                    // Check if the message is from the user
+                    const isUserMessage = message.type === 'user';
+                    // Determine if the previous message was from the same sender
+                    const sameSenderAsPrevious = index > 0 && message.type === messages[index - 1].type;
+                    // Determine if the next message is from a different sender
+                    const nextMessageDifferent = index < messages.length - 1 && message.type !== messages[index + 1].type;
+
+                    // We want to start a new container for the first message
+                    // or when the sender changes
+                    if (index === 0 || !sameSenderAsPrevious) {
+                        return (
+                            <Container key={index}>
+                                <Row>
+                                    <Col className={`message ${message.type}`}>
+                                        <br/>{message.text}
+                                    </Col>
+                                </Row>
+                                {/* If the next message is from a different sender or it's the last message, we close the container */}
+                                {nextMessageDifferent || index === messages.length - 1 ? null : <br />}
+                            </Container>
+                        );
+                    } else {
+                        // If the message is not the first and the sender hasn't changed,
+                        // we continue in the same container without adding a new one.
+                        return (
+                            <React.Fragment key={index}>
+                                <Row>
+                                    <Col className={`message ${message.type}`}>
+                                        {message.text}
+                                    </Col>
+                                </Row>
+                                {/* We only add a bottom margin if the next message is from a different sender */}
+                                {nextMessageDifferent ? null : <br />}
+                            </React.Fragment>
+                        );
+                    }
+                })}
+                {/* {messages.map((message, index) => {
+
+                    console.log(message)
+                    return (
+                        <Container>
+                            {
+                                message.type == 'user' ? <Row>
+                                    <Col>
+                                        <div key={index} className={`message user`}>
+                                            <br />{message.text}
+                                        </div>
+                                    </Col>
+                                </Row> : null
+                            }
+                            <br />
+                            <Row>
+                                <Col>
+                                    {
+                                        message.type == 'bot' ? <div key={index} className={`message bot`}>
+                                            <br />{message.text}
+                                        </div> : null
+                                    }
+
+                                </Col>
+
+                            </Row>
+                        </Container>
+                    )
+                })} */}
                 {isTyping && (
                     <div className="typing-indicator">
                         <span></span>
@@ -119,10 +184,10 @@ function ChatBot() {
             </div>
         </div>
 
-            /* <div className="input-container">
-                 <input value={input} onChange={e => setInput(e.target.value)} placeholder="Ask me anything..." />
-                 <button onClick={handleSubmit}>Submit</button>
-             </div> */
+        /* <div className="input-container">
+             <input value={input} onChange={e => setInput(e.target.value)} placeholder="Ask me anything..." />
+             <button onClick={handleSubmit}>Submit</button>
+         </div> */
 
         // </>
 
