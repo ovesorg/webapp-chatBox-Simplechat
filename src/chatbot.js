fdee02b2447acb617c436a5ccaa09bcf28d6f540
mary_waiyego <waiyegomary55@gmail.com>
2023-11-20 09:48:34 +0300

    monday embedd

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
+import chatbotIcon from './imagess/chatbot-icon.jpg';
+import userIcon from './imagess/user.png';
+import botIcon from './imagess/ovesLogoimage.png';
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './chatbot.css';
 import 'react-chat-elements/dist/main.css'
-import {MessageBox} from 'react-chat-elements';
+import { MessageBox } from 'react-chat-elements';
 // import { ChatList } from "react-chat-elements";
 //import { MessageList } from "react-chat-elements";
 
@@ -44,10 +47,15 @@ function ChatBot() {
     }, [messages]);
 
     useEffect(() => {
-    window.toggleChatbot = () => {
+        window.toggleChatbot = () => {
+            setIsOpen(!isOpen);
+        };
+    }, [isOpen]);
+
+    const toggleChatbot = () => {
         setIsOpen(!isOpen);
     };
-    }, [isOpen]);
+
 
 
     const handleKeyDown = (e) => {
@@ -70,54 +78,65 @@ function ChatBot() {
         }
     };
 
-    const styles={
-        marginTop:"5px"
-
+    const styles = {
+        marginTop: "5px",
     }
+
+    console.log(isOpen)
     return (
-        <div className={"chatbot-container $ {isOpen ? 'open' : 'closed'}"}>
-            <div className="chatbot-header">Topic: {topic}</div>
-            <div className="messages-container">
-                {messages.map((message, index) => (
-                    <div key={index} className={`message-row ${message.type}`}>
-                        <div className={`message ${message.type}`}>
-                            {/* {message.text} */}
-                            {message.type === "bot" ?
-                             <MessageBox
-                                position="left"
-                                title='OvSmart'
-                                type="text"
-                                text={message.text}
-                                    date={new Date() }// Use the timestamp from the message
-                             />
-                            : <MessageBox
-                                    styles={styles}
-                                    position='right'
-                                    title={message.type}
-                                    titleColor='grey'
-                                    type='text'
-                                    text={message.text}
-                                    date={new Date()}
-                              />
-                            }
-
-                        </div>
+        <>
+            <div id="chatbot-icon-container" onClick={toggleChatbot}>
+                <img src={chatbotIcon} alt="Chat Icon" width={40} />
+            </div>
+            {
+                isOpen ? <div className={isOpen ? "chatbot-container open" : "chatbot-container closed"}>
+                    <div className="chatbot-header">
+                        Topic: {topic}
+                        <span className="chatbot-close-button" onClick={toggleChatbot}>X</span>
                     </div>
-                ))}
-                <div ref={messagesEndRef} />
-            </div>
-            <div className="input-container">
-                <input
-                    value={input}
-                    onChange={e => setInput(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Ask me anything..."
-                />
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
+                    <div className="messages-container">
+                        {messages.map((message, index) => (
+                            <div key={index} className={`message-row ${message.type}`}>
+                                <div className={`message ${message.type}`}>
+                                    {message.type === "bot" ?
+                                        <MessageBox
+                                            avatar={botIcon}
+                                            position="left"
+                                            title='OvSmart'
+                                            type="text"
+                                            text={message.text}
+                                            date={new Date()}// Use the timestamp from the message
+                                        />
+                                        : <MessageBox
+                                            avatar={userIcon}
+                                            styles={styles}
+                                            position='right'
+                                            title={message.type}
+                                            titleColor='grey'
+                                            type='text'
+                                            text={message.text}
+                                            date={new Date()}
+                                        />
+                                    }
 
-         </div>
+                                </div>
+                            </div>
+                        ))}
+                        <div ref={messagesEndRef} />
+                    </div>
+                    <div className="input-container">
+                        <input
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                            placeholder="Ask me anything..."
+                        />
+                        <button onClick={handleSubmit}>Submit</button>
+                    </div>
 
+                </div> : null
+            }
+        </>
     );
 }
 
