ef0033e48d66ce73c6b26511fccee4f86232330d
Dennis <dennisndegwa001@gmail.com>
2023-12-13 08:06:26 +0300

    Bot

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -50,6 +50,7 @@ export default function AuthentificationPage(props) {
                     .catch((err) => console.log(err));
             }
         },
+        // eslint-disable-next-line
         [user]
     );
 
@@ -62,17 +63,27 @@ export default function AuthentificationPage(props) {
             <div className="auth-header">
                 <div className="container">
                     {/* {page === 'signin' ? <h1>Login </h1> : <h1>Sign Up</h1>} */}
-                    <h1> <img style={{height: "30px", width: "30px", marginBottom: "7px", marginRight: "10px"}} src={logo} /> Hi there! <Emoji symbol="👋"/></h1>
+                    <h1> <img style={{height: "30px", width: "30px", marginBottom: "7px", marginRight: "10px"}} src={logo} alt="logo" /> Hi there! <Emoji symbol="👋"/></h1>
                     <p>Welcome to Omnivoltaic Assist.</p>
                 </div>
             </div>
             <div className="content-container">
-                <button onClick={() => login()} className={page === 'signin' ? "google-login" : "google-signup"}>
+                <div className="message-box">
+                    <div className="top">
+                        <p>
+Hi!<Emoji symbol="👋"/> What brings you here today?
+                        Please Login so that we can answer your questions</p>
+                    </div>
+                    <div className="content">
+                     <button onClick={() => login()} className={page === 'signin' ? "google-login" : "google-signup"}>
                     <svg style={{ marginRight: "12px" }} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
                         <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
                     </svg>
                     {page === "signin" ? "Login" : "Sign Up"} with Google
-                </button>
+                </button> 
+                </div>
+                </div>
+
             </div>
             <p className="footer-text">{page === "signin" ? "Don't" : "Already"} have an account? <span onClick={handlePageChange} className="auth-text">{page === "signin" ? "Sign Up" : "Sign In"}</span></p>
         </>
